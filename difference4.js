928b770d24a2b0d54ab07f2036998e2432cc8a8a
orstavik <orstavik77@gmail.com>
2025-07-06 20:40:46 +0200

    bs

--- difference4.js
+++ difference4.js
@@ -16,7 +16,7 @@ export function levenshteinMinimalShifts(A, B) {
   return table;
 }
 
-function* backtrace(table, A, B) {
+export function* backtrace(table, A, B) {
   const W = B.length + 1;
   let n = table.length - 1;
   while (n >= 0) {
@@ -82,133 +82,55 @@ export function diff(A, B) {
   throw new Error("No end found in backtrace");
 }
 
-const GRID_STYLE = `
-<style>
-  div.mono {
-    display: grid;
-    grid-auto-rows: 1fr;
-    grid-auto-columns: auto;
-    grid-auto-flow: row;
-    font-family: monospace;
-    text-align: center;
-    width: max-content;
-  }
 
-  div.mono > div {
-    border: 1px solid skyblue;
-    padding: 0.2em;
-    margin: 0;
-    aspect-ratio: 1 / 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-  }
-  div.mono > div:is(.del,.add,.cross,.match):hover::before {
-    content: attr(a) " / " attr(b);
-    position: absolute;
-    bottom: 80%;
-    left: 80%;
-    background-color: yellow;
-    z-index: 1000;
-    width: max-content;
-    padding: 0.2em;
-    border: 1px dotted darkgrey;
-  }
-  div.mono > div.del { background-color: salmon; }
-  div.mono > div.add { background-color: plum; }
-  div.mono > div.cross { background-color: chocolate; }
-  div.mono > div.match { background-color: lime; }
-  div.mono > div.end { background-color: grey; }
-</style>`;
-document.head.insertAdjacentHTML("beforeend", GRID_STYLE);
+// export function diffAsArray(A, B) {
+//   const table = levenshteinMinimalShifts(A, B);
+//   const res = [];
+//   let now, i = table.length - 1, j = table[0].length - 1;
+//   while (i > 0 && j > 0 && (now = table[i][j])) {
+//     const equals = now & 0xFF;
+//     if (equals) {
+//       i -= equals;
+//       j -= equals;
+//       const a = A.slice(i, i + equals);
+//       res.unshift({ a });
+//     } else {
+//       const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
+//       if (!res[0]?.b) res.unshift({ a: [], b: [] });
+//       if ((topLeft >= top && topLeft >= left) || top >= left)
+//         res[0].a.unshift(A[--i]);
+//       if ((topLeft >= top && topLeft >= left) || left > top)
+//         res[0].b.unshift(B[--j]);
+//     }
+//   }
+//   if (i || j)
+//     res.unshift({ a: A.slice(0, i), b: B.slice(0, j) });
+//   return res;
+// }
 
-
-function printGrid(rows, bt, actions, A, B) {
-
-  const grid = `<div class="mono" style="grid-template-columns: repeat(${B.length + 2}, max-content);">
-      ${Array.from(rows).map(n => `<div a="" b="">${n >> 16}.${n & 0xFFFF}</div>`).join("")}
-    </div>`;
-  document.body.insertAdjacentHTML("beforeend", grid);
-  const children = document.body.lastElementChild.children;
-
-  for (let p of bt) {
-    children[p.n].classList.add(p.type);
-    p.a && children[p.n].setAttribute("a", p.a);
-    p.b && children[p.n].setAttribute("b", p.b);
-  }
-  children[0].insertAdjacentHTML("beforebegin", (" " + B).split("").map(c => `<div>${c}</div>`).join(""));
-  A = "  " + A;
-  for (let i = 0; i < A.length; i++)
-    children[i * (B.length + 2)].insertAdjacentHTML("beforebegin", `<div>${A[i]}</div>`);
-  document.body.insertAdjacentHTML("beforeend",
-    `<ul class="actions">
-      ${actions.map(({ a, b }) => `<li>${a == b ? "=" : !a ? "+" : !b ? "-" : "/"}  ${a} / ${b}</li>`).join("\n")}
-    </ul>`);
-}
-
-function test(A, B) {
-  const table = levenshteinMinimalShifts(A, B);
-  const bt = [...backtrace(table, A, B)];
-  const actions = diff(A, B);
-  printGrid(table, bt, actions, A, B);
-}
-
-test("aby", "abx");
-test("ab", "ab_a_bxx");
-test("ab", "a_b_abxx");
-test("ab_a_bx", "ab");
-test("a_b_abx", "ab");
-test("xxa_b_abx", "ab");
-
-export function diffAsArray(A, B) {
-  const table = levenshteinMinimalShifts(A, B);
-  const res = [];
-  let now, i = table.length - 1, j = table[0].length - 1;
-  while (i > 0 && j > 0 && (now = table[i][j])) {
-    const equals = now & 0xFF;
-    if (equals) {
-      i -= equals;
-      j -= equals;
-      const a = A.slice(i, i + equals);
-      res.unshift({ a });
-    } else {
-      const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
-      if (!res[0]?.b) res.unshift({ a: [], b: [] });
-      if ((topLeft >= top && topLeft >= left) || top >= left)
-        res[0].a.unshift(A[--i]);
-      if ((topLeft >= top && topLeft >= left) || left > top)
-        res[0].b.unshift(B[--j]);
-    }
-  }
-  if (i || j)
-    res.unshift({ a: A.slice(0, i), b: B.slice(0, j) });
-  return res;
-}
-
-export function diffAsStr(A, B) {
-  const table = levenshteinMinimalShifts(A, B);
-  const res = [];
-  let now, i = table.length - 1, j = table[0].length - 1;
-  while (i >= 0 && j >= 0 && (now = table[i][j])) {
-    const equals = now & 0xFF;
-    if (equals) {
-      i -= equals;
-      j -= equals;
-      const str = A.slice(i, i + equals).join("");
-      res.unshift({ a: str, b: str });
-    } else {
-      const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
-      const a = (topLeft >= top && topLeft >= left) || top >= left ? A[--i] : "";
-      const b = (topLeft >= top && topLeft >= left) || left > top ? B[--j] : "";
-      res[0]?.a == res[0]?.b ? res.unshift({ a, b }) :
-        (res[0].a = a + res[0].a, res[0].b = b + res[0].b);
-    }
-  }
-  if (i || j)
-    res.unshift({ a: A.slice(0, i).join(""), b: B.slice(0, j).join("") });
-  return res;
-}
+// export function diffAsStr(A, B) {
+//   const table = levenshteinMinimalShifts(A, B);
+//   const res = [];
+//   let now, i = table.length - 1, j = table[0].length - 1;
+//   while (i >= 0 && j >= 0 && (now = table[i][j])) {
+//     const equals = now & 0xFF;
+//     if (equals) {
+//       i -= equals;
+//       j -= equals;
+//       const str = A.slice(i, i + equals).join("");
+//       res.unshift({ a: str, b: str });
+//     } else {
+//       const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
+//       const a = (topLeft >= top && topLeft >= left) || top >= left ? A[--i] : "";
+//       const b = (topLeft >= top && topLeft >= left) || left > top ? B[--j] : "";
+//       res[0]?.a == res[0]?.b ? res.unshift({ a, b }) :
+//         (res[0].a = a + res[0].a, res[0].b = b + res[0].b);
+//     }
+//   }
+//   if (i || j)
+//     res.unshift({ a: A.slice(0, i).join(""), b: B.slice(0, j).join("") });
+//   return res;
+// }
 
 // export function diff(A, B) {
 //   const table = levenshteinLengthWeight(A, B);
