fbeb4271a82cb8f61ef60048b2aca9485e4ab4fc
orstavik <orstavik77@gmail.com>
2025-07-07 11:53:05 +0200

    bs

---
+++ testset2.js
@@ -0,0 +1,62 @@
+export default [
+  [
+    `<div template:="" i::fetch:embrace="SerSeaEpi.json">
+  <h2>List of Series ("series" is an array? true)</h2>
+  <ol>
+    <li>
+      <h3>Batman The Series is the no. 0 in your play list.</h3>
+      <ul>
+        <li>
+          <h4>Hjelp! Batman is the no. 0 in series Batman The Series.</h4>
+          <ol> <!-- { "type": "season", "title": "Hjelp! Batman", "episodes": [ "episode_pilot" ] } -->
+            <li>episode #0: episode_pilot produced by DC Comics.</li> <template for="episode of season.episodes">
+              <!-- {{JSON.stringify(season, null, 2)}} -->
+              <li>episode #{{#episode}}: {{episode}} produced by {{producer}}.</li>
+            </template>
+          </ol> <template if="(#season % 2) === 0">
+            <ol> <template for="episode of season.episodes"> <!-- {{JSON.stringify(season, null, 2)}} -->
+                <li>episode #{{#episode}}: {{episode}} produced by {{producer}}.</li>
+              </template></ol>
+          </template>
+        </li>
+        <li>
+          <h4>Hjelp! Bat2 is the no. 1 in series Batman The Series.</h4> <template if="(#season % 2) === 0">
+            <ol> <template for="episode of season.episodes"> <!-- {{JSON.stringify(season, null, 2)}} -->
+                <li>episode #{{#episode}}: {{episode}} produced by {{producer}}.</li>
+              </template></ol>
+          </template>
+        </li>
+        <li>
+`, `
+<div template:="" i::fetch:embrace="SerSeaEpi.json">
+  <h2>List of Series ("series" is an array? true)</h2>
+  <ol>
+    <li>
+      <h3>Batman The Series is the no. 0 in your play list.</h3>
+      <ul>
+        <li>
+          <h4>Hjelp! Batman is the no. 0 in series Batman The Series.</h4>
+          <ol> <!-- { "type": "season", "title": "Hjelp! Batman", "episodes": [ "episode_pilot" ] } -->
+            <li>episode #0: episode_pilot produced by DC Comics.</li> <template for="episode of season.episodes">
+              <!-- {{JSON.stringify(season, null, 2)}} -->
+              <li>episode #{{#episode}}: {{episode}} produced by {{producer}}.</li>
+            </template>
+          </ol> <template if="(#season % 2) === 0">
+            <ol> <template for="episode of season.episodes"> <!-- {{JSON.stringify(season, null, 2)}} -->
+                <li>episode #{{#episode}}: {{episode}} produced by {{producer}}.</li>
+              </template></ol>
+          </template>
+        </li>
+        <li>
+          <h4>Hjelp! Bat2 is the no. 1 in series Batman The Series.</h4> <template if="(#season % 2) === 0">
+            <ol> <template for="episode of season.episodes">
+                <!-- {{JSON.stringify(season, null, 2)}} -->
+                <li>episode #{{#episode}}: {{episode}} produced by {{producer}}.</li>
+              </template></ol>
+          </template>
+        </li>
+        <li>
+    `,
+    [[0, 1], [1166, 1166], [0, 16], [212, 212], [0, 4]]
+  ]
+]
