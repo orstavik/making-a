c2fbdc93b77a1ecbfcbfba0154df879e5916e116
orstavik <orstavik77@gmail.com>
2025-07-07 19:48:54 +0200

    version 8

--- difference.js
+++ difference.js
@@ -1,84 +1,123 @@
-export function levenshteinLengthWeight(A, B) {
-  const height = A.length;
-  const width = B.length;
-  const table = Array.from({ length: height + 1 }, _ => Array(width + 1).fill(0));
-  for (let i1 = 0, i2 = 1; i1 < height; i1++, i2++)
-    for (let j1 = 0, j2 = 1; j1 < width; j1++, j2++)
-      table[i2][j2] = A[i1] === B[j1] ?
-        Math.max(table[i2][j1], table[i1][j2], table[i1][j1] + (1 << 22) + (Math.pow(table[i1][j1] & 0xFF, 2) << 8) + 1) :
-        Math.max(table[i2][j1], table[i1][j2]) & ~0xFF;
-  return table;
-}
+const EDIT = 1 << 16;
+const STREAKEND = 1;
 
-export function diffAsArray(A, B) {
-  const table = levenshteinLengthWeight(A, B);
-  const res = [];
-  let now, i = table.length - 1, j = table[0].length - 1;
-  while (i > 0 && j > 0 && (now = table[i][j])) {
-    const equals = now & 0xFF;
-    if (equals) {
-      i -= equals;
-      j -= equals;
-      const a = A.slice(i, i + equals);
-      res.unshift({ a });
-    } else {
-      const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
-      if (!res[0]?.b) res.unshift({ a: [], b: [] });
-      if ((topLeft >= top && topLeft >= left) || top >= left)
-        res[0].a.unshift(A[--i]);
-      if ((topLeft >= top && topLeft >= left) || left > top)
-        res[0].b.unshift(B[--j]);
-    }
-  }
-  if (i || j)
-    res.unshift({ a: A.slice(0, i), b: B.slice(0, j) });
+export function levenshteinMinimalShifts(A, B) {
+  const H = A.length, W = B.length, H2 = H + 1, W2 = W + 1;
+  const res = new Uint32Array(H2 * W2);
+  for (let i = 1; i < W2; i++) res[i] = i * EDIT + (i - 1) * STREAKEND;
+  for (let i = 1; i < H2; i++) res[i * W2] = i * EDIT + (i - 1) * STREAKEND;
+  for (let y1 = 0, y2 = 1; y1 < H; y1++, y2++)
+    for (let x1 = 0, x2 = 1; x1 < W; x1++, x2++)
+      res[y2 * W2 + x2] = Math.min(
+        res[y1 * W2 + x2] + EDIT + STREAKEND,
+        res[y2 * W2 + x1] + EDIT + STREAKEND,
+        res[y1 * W2 + x1] + (A[y1] != B[x1] ? EDIT + STREAKEND : A[y2] != B[x2] || y2 == H ? STREAKEND : 0)
+      );
   return res;
 }
 
-export function diffAsStr(A, B) {
-  const table = levenshteinLengthWeight(A, B);
-  const res = [];
-  let now, i = table.length - 1, j = table[0].length - 1;
-  while (i > 0 && j > 0 && (now = table[i][j])) {
-    const equals = now & 0xFF;
-    if (equals) {
-      i -= equals;
-      j -= equals;
-      const str = A.slice(i, i + equals).join("");
-      res.unshift({ a: str, b: str });
-    } else {
-      const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
-      const a = (topLeft >= top && topLeft >= left) || top >= left ? A[--i] : "";
-      const b = (topLeft >= top && topLeft >= left) || left > top ? B[--j] : "";
-      res[0]?.a == res[0]?.b ? res.unshift({ a, b }) :
-        (res[0].a = a + res[0].a, res[0].b = b + res[0].b);
+export function* backtrace(table, A, B) {
+  const W = B.length + 1;
+  let n = table.length - 1;
+  while (n > 0) {
+    const y = Math.floor(n / W);
+    const x = n % W;
+    let a = A[y - 1];
+    let b = B[x - 1];
+    const now = table[n];
+    let type;
+    if (y == 0)
+      type = "add";
+    else if (x == 0)
+      type = "del";
+    else {
+      let upLeft = table[n - W - 1];
+      if (upLeft == now) {
+        type = "match";
+      } else {
+        const up = table[n - W];
+        const left = table[n - 1];
+        upLeft -= EDIT;
+        if (up <= left && up <= upLeft)
+          type = "del";
+        else if (left <= up && left <= upLeft)
+          type = "add";
+        else if (a == b)
+          type = "match";
+        else
+          type = "cross";
+      }
+    }
+    if (type == "add") a = "";
+    if (type == "del") b = "";
+    yield { type, now, n, x, y, a, b };
+    if (type != "del") n -= 1;
+    if (type != "add") n -= W;
+  }
+}
+
+export function diffRaw(A, B) {
+  if (!A.length && !B.length) return [];
+  if (!A.length || !B.length) return [{ a: A, b: B }];
+  let p, res = [];
+  for (let { a, b } of backtrace(levenshteinMinimalShifts(A, B), A, B))
+    p && (p.a === p.b) === (a === b) ?
+      ((p.a = a + p.a), (p.b = b + p.b)) :
+      res.unshift(p = { a, b });
+  return res;
+}
+
+function mostCommonCharRegex(str) {
+  let freq = {}, winner = '', winnerVal = 0;
+  for (let c of str) {
+    const n = freq[c] = (freq[c] || 0) + 1;
+    if (n > winnerVal)
+      (winner = c), (winnerVal = n);
+  }
+  return winner.replace(/[.*+?^${}()|[\]\\]/g, '\\$&').replace(/\s/g, '\\s');
+}
+
+function findStartEnd(a, b) {
+  let start = 0, end = 0;
+  while (start < a.length && start < b.length && a[start] === b[start]) start++;
+  while (end < a.length - start && end < b.length - start && a[a.length - end - 1] === b[b.length - end - 1]) end++;
+  return { start, end };
+}
+
+function extractUnions(res) {
+  res[0].a !== res[0].b && res.unshift({ a: "", b: "" });
+  res.at(-1).a !== res.at(-1).b && res.push({ a: "", b: "" });
+  for (let i = 1; i < res.length; i += 2) {
+    const { a, b } = res[i];
+    const { start, end } = findStartEnd(a, b);
+    if (start) {
+      res[i - 1].a += a.slice(0, start);
+      res[i - 1].b += b.slice(0, start);
+    }
+    if (end) {
+      res[i + 1].a = a.slice(a.length - end) + res[i + 1].a;
+      res[i + 1].b = b.slice(b.length - end) + res[i + 1].b;
+    }
+    if (start || end) {
+      res[i].a = a.slice(start, a.length - end);
+      res[i].b = b.slice(start, b.length - end);
     }
   }
-  if (i || j)
-    res.unshift({ a: A.slice(0, i).join(""), b: B.slice(0, j).join("") });
+  !res[0].a && !res[0].b && res.shift();
+  !res.at(-1).a && !res.at(-1).b && res.pop();
   return res;
 }
 
 export function diff(A, B) {
-  const table = levenshteinLengthWeight(A, B);
-  const res = [];
-  let now, i = table.length - 1, j = table[0].length - 1;
-  while (i > 0 && j > 0 && (now = table[i][j])) {
-    const equals = now & 0xFF;
-    if (equals) {
-      i -= equals;
-      j -= equals;
-      const str = A.slice(i, i + equals);
-      res.unshift({ a: str, b: str });
-    } else {
-      const topLeft = table[i - 1][j - 1], top = table[i - 1][j], left = table[i][j - 1];
-      const a = (topLeft >= top && topLeft >= left) || top >= left ? A[--i] : "";
-      const b = (topLeft >= top && topLeft >= left) || left > top ? B[--j] : "";
-      res[0]?.a == res[0]?.b ? res.unshift({ a, b }) :
-        (res[0].a = a + res[0].a, res[0].b = b + res[0].b);
-    }
-  }
-  if (i || j)
-    res.unshift({ a: A.slice(0, i), b: B.slice(0, j) });
-  return res;
+  if ((A.length * B.length) < 1_000_000)
+    return diffRaw(A, B);
+  const regEx = new RegExp(`(${mostCommonCharRegex(A)}+)`, "g");
+  const AA = A.split(regEx);
+  const BB = B.split(regEx);
+  !AA[0] && AA.shift();
+  !BB[0] && BB.shift();
+  !AA.at(-1) && AA.pop();
+  !BB.at(-1) && BB.pop();
+  const res = diffRaw(AA, BB);
+  return extractUnions(res);
 }
